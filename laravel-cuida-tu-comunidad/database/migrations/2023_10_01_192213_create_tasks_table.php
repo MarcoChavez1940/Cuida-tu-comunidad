f19d7945b067d3331562bc586c2ca97abf2c5d2e
Marco Chavez <marcochavez1940@gmail.com>
2023-10-01 14:09:23 -0600

    create initial laravel project, create task and state model and migrations

---
+++ laravel-cuida-tu-comunidad/database/migrations/2023_10_01_192213_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('state_id')->index();
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+            $table->string('author');
+            $table->double('likes');
+
+            $table->foreign('state_id')->references('id')->on('states')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
